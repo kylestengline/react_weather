a81313586da2e970f50fab6c23ed99bc41408525
Kyle Stengline <kyle.stengline@gmail.com>
2017-02-19 12:00:13 -0800

    creates output for cities in weather reducer

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,3 +1,16 @@
-export default function(state = null, action) {
+import { FETCH_WEATHER } from '../actions/index';
+
+//putting inside array b/c we're expecting multiple cities
+//coming back
+export default function(state = [], action) {
+  switch(action.type) {
+    case FETCH_WEATHER: 
+      //insert new record with action.payload, then another record
+      //where ...state is.
+      //JSX syntax
+      return [ action.payload.data, ...state ]; 
+      //output from above: [ city, city, city ]
+  }
+
   return state;
 }

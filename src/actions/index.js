9d76e106945fb3d35e686229f863fa4892d3ca71
Kyle Stengline <kyle.stengline@gmail.com>
2017-02-18 23:35:38 -0800

    installs axios to handle request, creates custom url for api call

---
+++ src/actions/index.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${WEATHER_API}`; 
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  //take url we crafted, and make a get request on it.
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
+//payload is an optional property that goes along with actions 
+//that can contain some additional data that describes this particular action
+
+//backticks are ES6 syntax
+//thess are called template strings.
+//they take one string and a js variable, 
+//it injects that js variable into the string

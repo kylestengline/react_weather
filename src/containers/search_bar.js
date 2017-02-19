4691379328924ab850c225d60efd0e0bb448f395
Kyle Stengline <kyle.stengline@gmail.com>
2017-02-18 22:38:57 -0800

    updates search bar functionality for submitting

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -6,6 +6,8 @@ export default class SearchBar extends Component {
 
     this.state = { term: '' };
 
+  //if you have a callback that makes a reference to this,
+  //you need to bind it.
     this.onInputChange = this.onInputChange.bind(this);
   }
 
@@ -16,9 +18,14 @@ export default class SearchBar extends Component {
     this.setState({ term: event.target.value });
   }
 
+  //telling browser, "Don't Submit Form"
+  onFormSubmit(event) {
+    event.preventDefault();  
+  }
+
   render() {
     return (
-      <form className="input-group">
+      <form onSubmit={ this.onFormSubmit } className="input-group">
         <input 
           placeholder="Get a Five Day Forecast in your Favorite Cities"
           className="form-control"

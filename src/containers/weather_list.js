9603481f616afb0c667f4f8f5496a989368737b1
Kyle Stengline <kyle.stengline@gmail.com>
2017-02-19 16:05:09 -0800

    adds sparklines and updates weather list output with graphs

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
@@ -8,10 +9,17 @@ class WeatherList extends Component {
     //we must add the key to the top level element
     //which has to be a unique piece of data.
     const name = cityData.city.name;
+    const temps = cityData.list.map(weather => weather.main.temp);
+    console.log(temps);
 
     return (
       <tr key={ name }>
         <td>{ name }</td>
+        <td>
+          <Sparklines height={120} width={180} data={temps}>
+            <SparklinesLine color="red" />
+          </Sparklines>
+        </td>
       </tr>
     ); 
   }

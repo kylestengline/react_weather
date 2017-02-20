9da813d14231f434a04a77fb18b45f395b84a01a
Kyle Stengline <kyle.stengline@gmail.com>
2017-02-19 16:38:08 -0800

    updates chart to show relevant data, converts kelvin to celsius

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -9,8 +9,9 @@ class WeatherList extends Component {
     //we must add the key to the top level element
     //which has to be a unique piece of data.
     const name = cityData.city.name;
-    const temps = cityData.list.map(weather => weather.main.temp);
-    console.log(temps);
+    const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => temp - 273);
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+    const humidities = cityData.list.map(weather => weather.main.humidity);
 
     //if you find yourself replicating markup,
     //like we did with generating the chart with sparklines
@@ -18,9 +19,9 @@ class WeatherList extends Component {
     return (
       <tr key={ name }>
         <td>{ name }</td>
-        <td>
-          <Chart data={ temps } color="orange" />
-        </td>
+        <td><Chart data={ temps } color="green" units="C" /></td>
+        <td><Chart data={ pressures } color="red" units="hPa" /></td>
+        <td><Chart data={ humidities } color="black" units="%" /></td>
       </tr>
     ); 
   }
@@ -31,9 +32,9 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Temperature (C)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
           </tr>
         </thead>
         <tbody>
